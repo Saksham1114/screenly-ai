9277eac954f144a3adb43d61a92c852c7f7b2b93
Saksham1114 <sakshamkumar1109@gmail.com>
2026-03-24 23:31:23 +0530

    Fixed port issue for deployment

--- app.py
+++ app.py
@@ -236,4 +236,5 @@ def download():
 # ---------------- RUN ---------------- #
 
 if __name__ == "__main__":
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
